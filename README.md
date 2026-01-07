fa91ef40eb0b3f0d902c57f9e5994d2bddf006fb
toderodavi <estrela.alem@gmail.com>
2026-01-07 01:59:33 -0300

    testing/initial-setup: Initial templated, prob will be deleted for typescript implementation

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-The first commit! A simple README!
+# TODO
+
+- Create 'teleport spaces', consited by two spaces delimited by the GM for tokens who
+  enter those spaces be teleported to the other end and vice-versa
+
+To scaffold a tool, you need to build: OBR.tool.create, OBR.tool.mode and OBR.tool.action.
+
+tool.create = creates the tool in the left side bar
+tool.mode = creates the mode the tool will operate (the mode where a action will be performed)
+tool.action = creates the action the tool will perform
+
+Interaction API = Show in real-time the user actions (as usign the tool)
+Item API = Consolidate the user action as a item (????)
---
+++ src/main.js
@@ -0,0 +1,103 @@
+import { setupCounter } from './setupCounter'
+import './style.css'
+import OBR, { buildLine } from '@owlbear-rodeo/sdk'
+
+const ID = 'com.wormhole.extension'
+
+function createTool() {
+  OBR.tool.create({
+    id: `${ID}/tool`,
+    icons: [
+      {
+        icon: '/line.svg',
+        label: 'Line tool',
+      },
+    ],
+    defaultMetadata: {
+      strokeColor: 'red',
+    },
+  })
+}
+
+function createMode() {
+  let interaction = null
+  OBR.tool.createMode({
+    id: `${ID}/mode`,
+    icons: [
+      {
+        icon: '/line.svg',
+        label: 'Line',
+        filter: {
+          activeTools: [`${ID}/tool`],
+        },
+      },
+    ],
+    async onToolDragStart(context, event) {
+      let strokeColor = 'red'
+      // Define um valor padrão para cor
+      // e depois checa se o localStorage possui um valor
+      // se houver, utiliza ele
+      if (typeof context.metadata.strokeColor === 'string ') {
+        strokeColor = context.metadata.strokeColor
+      }
+      const line = buildLine().startPosition(event.pointerPosition).endPosition(event.pointerPosition).strokeColor(strokeColor).build()
+      interaction = await OBR.interaction.startItemInteraction(line)
+    },
+    onToolDragMove(_, event) {
+      if (interaction) {
+        const [update] = interaction
+        update((line) => {
+          line.endPosition = event.pointerPosition
+        })
+      }
+    },
+    onToolDragEnd(_, event) {
+      if (interation) {
+        const [update, stop] = interaction
+        const line = update((line) => {
+          line.endPosition = event.pointerPosition
+        })
+        OBR.scene.items.addItems([line])
+        stop()
+      }
+      interaction = null
+    },
+    onToolDragCancel() {
+      if (interaction) {
+        const [_, event] = interaction
+        stop()
+      }
+      interaction = null
+    },
+  })
+}
+
+function createAction() {
+  OBR.tool.createAction({
+    id: `${ID}/action`,
+    icons: [
+      {
+        icon: '/icon.svg',
+        label: 'Color',
+        filter: {
+          activeTools: [`${ID}/tool`],
+        },
+      },
+    ],
+  })
+}
+
+OBR.onReady(() => {
+  createTool()
+  createMode()
+  createAction()
+})
+
+document.querySelector('#app').innerHTML = `
+  <div>
+    <h1>OI FERNANDA</h1>
+    <p>Um teste da extensão</p>
+    <button id="button">DIOWJAODIAJW</button>
+  </div>
+`
+setupCounter(document.querySelector('#button'))
